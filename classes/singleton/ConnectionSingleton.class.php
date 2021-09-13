c54683b5401fdf4c03d9188f0c575213958d276b
Rafael Teixeira <rafaeldemeirateixeira@gmail.com>
2021-09-13 15:46:50 -0300

    Conexão com a base de teste

--- classes/singleton/ConnectionSingleton.class.php
+++ classes/singleton/ConnectionSingleton.class.php
@@ -14,7 +14,7 @@ class ConnectionSingleton
     public static function get()
     {
         if (!isset(self::$connection)) {
-            self::$connection = new PDO('mysql:host=mysql:3306;dbname=receiv', 'root', 'secret', array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+            self::$connection = new PDO('mysql:host=l0ebsc9jituxzmts.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306;dbname=xrg41l9lpsj92cih', 'q3sj1x3qm4fud7uc', 't141se23vuhve4qq', array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
             self::$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             self::$connection->setAttribute(PDO::ATTR_ORACLE_NULLS, PDO::NULL_EMPTY_STRING);
         }
